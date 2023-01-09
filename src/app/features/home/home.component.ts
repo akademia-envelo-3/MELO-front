c4e7459e14ddb12bf61bec9e83ee3fafb6e9db09
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2023-01-09 15:13:59 +0100

    feat: deleted tailwind, moved design system to scss

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,13 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <div class="bg-gradient-green-2">Application shell</div>
-    <router-outlet></router-outlet>
-  `,
+  imports: [RouterOutlet, MatInputModule],
+
+  template: ` <router-outlet></router-outlet> `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
