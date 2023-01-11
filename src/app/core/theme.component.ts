17eace56893f302045d5587cbeecbb89da0e76fa
Rafal Fuz <rafalfuz@gmail.com>
2023-01-11 15:41:41 +0100

    feat: created power icon

--- src/app/core/theme.component.ts
+++ src/app/core/theme.component.ts
@@ -8,9 +8,22 @@ import { PowerIconComponent } from "../shared/ui/power-icon/power-icon.component
   template: `
     <h1>Storybook-like route</h1>
     <p>power-icon</p>
-    <app-power-icon class="theme-primary"></app-power-icon>
-    <app-power-icon class="theme-secondary"></app-power-icon>
-    <app-power-icon class="theme-teriarty"></app-power-icon>
+    <app-power-icon class="theme-primary" [amountOfPeople]="21"></app-power-icon>
+    <app-power-icon class="theme-secondary" [limitOfPeople]="999"></app-power-icon>
+    <app-power-icon
+      class="theme-teriarty"
+      [amountOfPeople]="999"
+      [limitOfPeople]="999"
+    ></app-power-icon>
+    <app-power-icon
+      class="theme-teriarty"
+      width="140px"
+      height="140px"
+      fontSize="24px"
+      iconFontSize="50px"
+      [amountOfPeople]="999"
+      [limitOfPeople]="999"
+    ></app-power-icon>
   `,
 })
 export default class ThemeComponent {}
