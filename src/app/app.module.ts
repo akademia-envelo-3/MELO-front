0cc2d23976b46ec8bc664f1bc75bb463ccea06be
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2023-01-11 11:44:42 +0100

    feat: changed prettier rule to singleQuote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { StoreModule } from "@ngrx/store";
-import { EffectsModule } from "@ngrx/effects";
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
-import { AppComponent } from "./app.component";
-import { API_URL, IS_PRODUCTION } from "@core/env.token";
-import { environment } from "src/environment";
-import { RouterModule } from "@angular/router";
-import { noProductionGuard } from "@shared/no-production.guard";
-import { EventModule } from "./features/event";
-import { CustomHttpInterceptor } from "./core";
+import { AppComponent } from './app.component';
+import { API_URL, IS_PRODUCTION } from '@core/env.token';
+import { environment } from 'src/environment';
+import { RouterModule } from '@angular/router';
+import { noProductionGuard } from '@shared/no-production.guard';
+import { EventModule } from './features/event';
+import { CustomHttpInterceptor } from './core';
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,24 +24,24 @@ import { CustomHttpInterceptor } from "./core";
     BrowserAnimationsModule,
     RouterModule.forRoot([
       {
-        path: "",
+        path: '',
         children: [
           {
-            path: "",
-            loadChildren: () => import("./features/home/home.module"),
+            path: '',
+            loadChildren: () => import('./features/home/home.module'),
           },
           {
-            path: "auth",
-            loadChildren: () => import("./features/auth/auth.module"),
+            path: 'auth',
+            loadChildren: () => import('./features/auth/auth.module'),
           },
           {
-            path: "theme",
+            path: 'theme',
             canMatch: [noProductionGuard],
-            loadComponent: () => import("./core/theme.component"),
+            loadComponent: () => import('./core/theme.component'),
           },
           {
-            path: "**",
-            redirectTo: "",
+            path: '**',
+            redirectTo: '',
           },
         ],
       },
