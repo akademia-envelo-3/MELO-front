35c3d0ec21c92f0905c413990496518c2ecfe68a
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2023-01-11 15:34:23 +0100

    feat: created spinner

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { HomeComponent } from "./home.component";
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
-        path: '',
+        path: "",
         component: HomeComponent,
         children: [],
       },
