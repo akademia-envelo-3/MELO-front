4d4850aa980b5cd97dcd379b53358537f9c24e01
Rafal Fuz <rafalfuz@gmail.com>
2023-01-13 16:25:42 +0100

    fix: deleted power-icon component in theme view

--- src/app/core/theme.component.ts
+++ src/app/core/theme.component.ts
@@ -1,39 +1,22 @@
 import { Component } from '@angular/core';
-import { PowerIconComponent } from '../shared/ui/power-icon/power-icon.component';
 import { CircularButtonComponent } from '@shared/ui/circular-button/circular-button.component';
 
 @Component({
   selector: 'app-theme',
   standalone: true,
-  imports: [PowerIconComponent, CircularButtonComponent],
+  imports: [CircularButtonComponent],
   template: `
     <h1>Storybook-like route</h1>
-    <p>power-icon</p>
-    <app-power-icon class="theme-primary" [amountOfPeople]="21"></app-power-icon>
-    <app-power-icon class="theme-secondary" [limitOfPeople]="999"></app-power-icon>
-    <app-power-icon
-      class="theme-teriarty"
-      [amountOfPeople]="999"
-      [limitOfPeople]="999"
-    ></app-power-icon>
-    <app-power-icon
-      class="theme-teriarty"
-      width="140px"
-      height="140px"
-      fontSize="24px"
-      iconFontSize="50px"
-      [amountOfPeople]="999"
-      [limitOfPeople]="999"
-    ></app-power-icon>
+
     <div
       style="border: 1px solid white; padding: 20px; display: flex; justify-content: space-around"
     >
       <p style="text-align; color: white">Circular Buttons</p>
       <hr />
-      <app-circular-button size="sm">SM</app-circular-button>
-      <app-circular-button size="md">MD</app-circular-button>
-      <app-circular-button size="lg">LG</app-circular-button>
-      <app-circular-button size="xl">XL</app-circular-button>
+      <app-circular-button size="sm"></app-circular-button>
+      <app-circular-button size="md"></app-circular-button>
+      <app-circular-button size="lg"></app-circular-button>
+      <app-circular-button size="xl"></app-circular-button>
       <app-circular-button icon="done" size="sm"></app-circular-button>
       <app-circular-button icon="done" size="md"></app-circular-button>
       <app-circular-button icon="done" size="lg"></app-circular-button>
