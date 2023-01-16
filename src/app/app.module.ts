921a393b014bc9b606fc288de3bf46acf347c2b5
Dominika Papierska <44174415+dominikapap@users.noreply.github.com>
2023-01-16 14:13:01 +0100

    FE 2  Rectangular Buttons (#37)
    * feat: deafult button component
    
    created default button component in shared/ui folder, used component in app-theme
    
    * feat: added styles
    
    added styles to rect btns and cotainer in theme component
    
    * feat: rect buttons
    
    added index file, black and green variatons of btns, changed input from object to variables
    
    * fix: singleQuote
    
    changed singleQuote on true
    
    * feat: rectangular buttons
    
    removed unnecessary component and folders, moved style for buttons to styles.scss
    
    * feat: adjusted styles
    
    adjusted padding and box shadow
    
    * refactor: scss styles
    
    removed repeated styles and unnecassary empty lines
    
    * refactor: styles
    
    changed scss class name to be more readable, moved cursor:pointer to not disabled buttons
    
    * refactor: styles
    
    changed bg color for container, added a class for small button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +11,7 @@ import { RouterModule } from '@angular/router';
 import { noProductionGuard } from '@shared/no-production.guard';
 import { EventModule } from './features/event';
 import { CustomHttpInterceptor } from './core';
+
 
 @NgModule({
   declarations: [AppComponent],
