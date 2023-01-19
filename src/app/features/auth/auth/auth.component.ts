b2f7b9b07581841d88d4982335dbb385efeee118
abdevworks <czasacz@gmail.com>
2023-01-19 11:40:23 +0100

    feat: login-view
    Added login disabled button status change depending on form validation status

--- src/app/features/auth/auth/auth.component.ts
+++ src/app/features/auth/auth/auth.component.ts
@@ -1,9 +1,16 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { NgIf } from '@angular/common';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -13,11 +20,13 @@ import { NgIf } from '@angular/common';
   styleUrls: ['./auth.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit, OnDestroy {
   builder = inject(NonNullableFormBuilder);
   form = this.createForm();
   emailNotFocused = false;
   passwordNotFocused = false;
+  isFormValid = true;
+  sub = new Subscription();
 
   private createForm() {
     return this.builder.group({
@@ -49,5 +58,26 @@ export class AuthComponent {
 
   getPasswordErrorMessage() {
     return 'To pole jest obowiązkowe';
+  }
+
+  ngOnInit(): void {
+    this.sub = this.form.statusChanges.subscribe((status: string) => {
+      if (status === 'INVALID') {
+        this.onFormInvalid();
+      } else if (status === 'VALID') {
+        this.onFormValid();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  private onFormValid() {
+    this.isFormValid = false;
+  }
+  private onFormInvalid() {
+    this.isFormValid = true;
   }
 }
