b083354dff8c113a13655a48536ba61b47c15e43
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2023-01-20 11:34:09 +0100

    feat: add initial dropdown styles and html

--- src/app/features/ui/filter-search-button/filter-search-button.component.ts
+++ src/app/features/ui/filter-search-button/filter-search-button.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-filter-search-button',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './filter-search-button.component.html',
   styleUrls: ['./filter-search-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
