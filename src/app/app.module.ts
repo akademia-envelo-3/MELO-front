8c50ff71da58ba669d9031deb3c887eed7804ac1
Dominika Papierska <domi.papierska@gmail.com>
2023-01-20 11:43:42 +0100

    refactor: removed unnecessary imports
    removed material imports from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,6 @@ import { noProductionGuard } from '@shared/no-production.guard';
 import { EventModule } from './features/event';
 import { CustomHttpInterceptor } from './core';
 import { UnitFormComponent } from './features/unit/unit-form/unit-form.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
 import { UnitModule } from './features/unit/unit.module';
 
 @NgModule({
@@ -25,9 +20,6 @@ import { UnitModule } from './features/unit/unit.module';
     BrowserModule,
     HttpClientModule,
     EventModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     BrowserAnimationsModule,
@@ -65,8 +57,6 @@ import { UnitModule } from './features/unit/unit.module';
         ],
       },
     ]),
-    MatButtonModule,
-    ReactiveFormsModule,
     UnitModule,
   ],
   providers: [
