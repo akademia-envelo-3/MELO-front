fa5c125b053788f7875760b9234d06ce1fcc163a
Łukasz Staniszewski <l.staniszewski01@gmail.com>
2023-01-20 10:17:37 +0100

    Fe 9 spinner dots (#34)
    * feat: created spinner
    
    * feat: add spinner to theme component
    
    * fix: fixed scss variables, removed double quotes in home module
    
    * fix: changing inputs is modifying spinner correctly
    
    * fix: removed import from spinner styles file
    
    * fix: applied changes required in code review

--- src/app/core/theme.component.ts
+++ src/app/core/theme.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { SpinnerDotsComponent } from '@shared/index';
 import { CircularButtonComponent } from '@shared/ui/circular-button/circular-button.component';
 
 @Component({
   selector: 'app-theme',
   standalone: true,
-
-  imports: [CircularButtonComponent],
+  imports: [SpinnerDotsComponent, CircularButtonComponent],
   template: `
     <h1>Storybook-like route</h1>
-
+    <div>
+      <p class="text-h4">Spinner Dots</p>
+      <app-spinner-dots size="md"></app-spinner-dots>
+    </div>
     <div class="circular-buttons-container bg-gradient-neutral-3">
       <p class="text-body-big">Circular Buttons</p>
       <div class="circular-buttons-container__btns">
