f98e9bd3ed16fcfd23122761bf0b1a89bbb3a623
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2023-01-20 15:15:30 +0100

    feat: add form group, assigned form groups in html

--- src/app/features/ui/filter-search-button/filter-search-button.component.ts
+++ src/app/features/ui/filter-search-button/filter-search-button.component.ts
@@ -1,12 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-filter-search-button',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './filter-search-button.component.html',
   styleUrls: ['./filter-search-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FilterSearchButtonComponent {}
+export class FilterSearchButtonComponent {
+  private fb = inject(NonNullableFormBuilder);
+
+  filtrSortForm = this.fb.group({
+    creationDate: ['', Validators.required],
+    startDate: ['', Validators.required],
+    timeToStartSort: ['', Validators.required],
+    creationDateSort: ['', Validators.required],
+  });
+
+  toggle(radioProp: HTMLInputElement) {
+    // console.log(radioProp);
+    // return (radioProp.checked = !radioProp.checked);
+  }
+}
