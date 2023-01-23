95274ffa4ec612a4fa192de49aeaa18d8c365056
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2023-01-23 11:10:05 +0100

    feat: approach with reactive forms, wasn't working, moved to passing input value through function

--- src/app/features/ui/filter-search-button/filter-search-button.component.ts
+++ src/app/features/ui/filter-search-button/filter-search-button.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface Test extends Omit<HTMLInputElement, 'name'> {
+  name: 'creationDate' | 'startDate' | 'timeToStart' | 'name';
+}
 @Component({
   selector: 'app-filter-search-button',
   standalone: true,
@@ -11,17 +14,45 @@ import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angula
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FilterSearchButtonComponent {
+  state = { creationDate: '', startDate: '', timeToStart: '', name: '' };
   private fb = inject(NonNullableFormBuilder);
 
+  creationDates = [
+    { name: 'creationDate', value: 'Dzisiaj', id: 'cd' },
+    { name: 'creationDate', value: 'Ten tydzień', id: 'ct' },
+    { name: 'creationDate', value: 'Ten miesiąc', id: 'cm' },
+    { name: 'creationDate', value: 'Ten rok', id: 'cr' },
+  ];
+  startDates = [
+    { name: 'startDate', value: 'Dzisiaj', id: 'sd' },
+    { name: 'startDate', value: 'Ten tydzień', id: 'st' },
+    { name: 'startDate', value: 'Ten miesiąc', id: 'sm' },
+    { name: 'startDate', value: 'Ten rok', id: 'sr' },
+  ];
+  sortByDates = [
+    { name: 'startDate', value: 'Rosnąco', id: 'sr' },
+    { name: 'startDate', value: 'Malejąco', id: 'sm' },
+  ];
+  sortByNames = [
+    { name: 'name', value: 'Rosnąco', id: 'nr' },
+    { name: 'name', value: 'Malejąco', id: 'nm' },
+  ];
+
+  sortOptions = [{ name: 'creationDate', value: 'Dzisiaj', id: 'cd' }];
   filtrSortForm = this.fb.group({
-    creationDate: ['', Validators.required],
-    startDate: ['', Validators.required],
-    timeToStartSort: ['', Validators.required],
-    creationDateSort: ['', Validators.required],
+    creationDate: [''],
+    startDate: [''],
+    timeToStartSort: [''],
+    name: [''],
   });
 
-  toggle(radioProp: HTMLInputElement) {
-    // console.log(radioProp);
-    // return (radioProp.checked = !radioProp.checked);
+  ngOnInit() {
+    this.filtrSortForm.valueChanges.subscribe(console.log);
+  }
+  toggleRadio(input: HTMLInputElement) {
+    // this.state = { ...this.state, [input.name]: input.value };
+    console.log(this.state);
+
+    return (input.checked = !input.checked);
   }
 }
