3cfac4f294b03e96a1c1d93af7ba763df500e1f7
abdevworks <czasacz@gmail.com>
2023-01-25 15:08:04 +0100

    feat: side-menu
    Added basic menu for mobile

--- src/app/features/ui/side-menu/side-menu.component.ts
+++ src/app/features/ui/side-menu/side-menu.component.ts
@@ -1,25 +1,56 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import { NgFor } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { NgFor, TitleCasePipe } from '@angular/common';
 
 export type SubCategory = {
   subCategoryName: string;
   isActive: boolean;
 };
 
+export type MenuCategory = {
+  categoryName: string;
+  categoryIcon: string;
+  subCategories: SubCategory[];
+};
+
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [MatListModule, NgFor],
+  imports: [
+    MatListModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatTooltipModule,
+    TitleCasePipe,
+    NgFor,
+  ],
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SideMenuComponent {
-  myLinks: SubCategory[] = [
-    { subCategoryName: 'wszystkie', isActive: true },
-    { subCategoryName: 'moje', isActive: false },
-    { subCategoryName: 'nowy +', isActive: false },
-    { subCategoryName: 'kategoria +', isActive: false },
+  menuCategories: MenuCategory[] = [
+    {
+      categoryName: 'wydarzenia',
+      categoryIcon: '../../../../assets/side-menu-icons/events.svg',
+      subCategories: [
+        { subCategoryName: 'wszystkie', isActive: true },
+        { subCategoryName: 'moje', isActive: false },
+        { subCategoryName: 'nowy +', isActive: false },
+        { subCategoryName: 'kategoria +', isActive: false },
+      ],
+    },
+    {
+      categoryName: 'koła zainteresowań',
+      categoryIcon: '../../../../assets/side-menu-icons/units.svg',
+      subCategories: [
+        { subCategoryName: 'wszystkie', isActive: true },
+        { subCategoryName: 'moje', isActive: false },
+        { subCategoryName: 'nowy +', isActive: false },
+      ],
+    },
   ];
 }
