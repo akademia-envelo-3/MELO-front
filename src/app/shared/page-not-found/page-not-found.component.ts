a74141a272c1e5e0a9bf23daa47bfc2ff8f9ef7c
Dominika Papierska <domi.papierska@gmail.com>
2023-01-26 15:05:32 +0100

    refactor: unnecessary imports
    removed unused imports

--- src/app/shared/page-not-found/page-not-found.component.ts
+++ src/app/shared/page-not-found/page-not-found.component.ts
@@ -1,5 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CircularButtonComponent } from '@shared/ui';
 
 @Component({
