2459f9cbd8d7fc51d485ded5e9876858fb2fa53c
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2023-01-27 14:03:56 +0100

    fix: removed leftovers in typescript file

--- src/app/features/unit/card/power-icon/unit-power-icon.ts
+++ src/app/features/unit/card/power-icon/unit-power-icon.ts
@@ -1,13 +1,6 @@
-import {
-  AfterContentInit,
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
-const iconSizes = ['xs', 'sm', 'md', 'lg'];
 type IconSizes = 'sm' | 'md' | 'lg';
-type SizeOptions = typeof iconSizes[number];
 
 @Component({
   selector: 'app-unit-power-icon',
@@ -16,26 +9,10 @@ type SizeOptions = typeof iconSizes[number];
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UnitPowerIconComponent {
-  fontSize: SizeOptions = 'md';
   @Input() memberNumber = 0;
   @Input() size: IconSizes = 'md';
-
-  // ngAfterContentInit() {
-  //   this.fontSize = this.calcFontSize();
-  // }
 
   get sizeClass() {
     return 'size--' + this.size;
   }
-
-  // private calcFontSize() {
-  //   if (this.memberNumber > 99) {
-  //     for (let i = 0; i < iconSizes.length; i++) {
-  //       if (iconSizes[i] === this.size) {
-  //         return iconSizes[i - 1];
-  //       }
-  //     }
-  //   }
-  //   return this.size;
-  // }
 }
