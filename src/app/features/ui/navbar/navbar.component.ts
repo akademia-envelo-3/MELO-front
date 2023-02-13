eb6d27de55c036d5093b6de5a8614441f1270e19
abdevworks <czasacz@gmail.com>
2023-02-13 11:24:25 +0100

    feat: merge-navbar-searchbar
    Partially added searchbar to navbar

--- src/app/features/ui/navbar/navbar.component.ts
+++ src/app/features/ui/navbar/navbar.component.ts
@@ -2,32 +2,13 @@ import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core
 import { Router } from '@angular/router';
 import { NgIf, NgClass } from '@angular/common';
 import { NotificationsQuantityComponent } from './notifications-quantity.component';
+import { SearchbarComponent } from '../searchbar-ui/searchbar/searchbar.component';
 
 @Component({
   standalone: true,
-  imports: [NotificationsQuantityComponent, NgIf, NgClass],
+  imports: [NotificationsQuantityComponent, NgIf, NgClass, SearchbarComponent],
   selector: 'app-navbar',
-  template: `
-    <nav class="navbar">
-      <img (click)="navToHome()" class="navbar__logoImg" src="../assets/app-logo.png" />
-      <div class="navbar__bell">
-        <app-notifications-quantity
-          *ngIf="notifications"
-          class="navbar__bell__notification"
-          >{{ notifications }}</app-notifications-quantity
-        >
-      </div>
-      <div
-        class="navbar__hamburger"
-        [ngClass]="{ change: this.menuActive }"
-        (click)="toggleMenu()"
-      >
-        <div class="navbar__hamburger__bar1"></div>
-        <div class="navbar__hamburger__bar2"></div>
-        <div class="navbar__hamburger__bar3"></div>
-      </div>
-    </nav>
-  `,
+  templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
