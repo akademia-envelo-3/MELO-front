1736b1a463ea7c517fc4e63366c28b2052dc8316
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2023-02-13 15:50:00 +0100

    fix: fixed import

--- src/app/features/ui/index.ts
+++ src/app/features/ui/index.ts
@@ -3,3 +3,4 @@ export * from './navbar/navbar.component';
 export * from './searchbar-ui/searchbar/searchbar.component';
 export * from './searchbar-ui/search.types';
 export * from './searchbar-ui/searchbar-dropdown/searchbar-dropdown.component';
+export * from './side-menu/side-menu.component';
