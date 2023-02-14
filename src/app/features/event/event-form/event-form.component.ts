3804c3e711c710b53b521a50f5b54041ab79da5b
Dominika Papierska <domi.papierska@gmail.com>
2023-02-14 13:55:18 +0100

    fix: merge issues
    removed faulty added files

--- src/app/features/event/event-form/event-form.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-event-form',
-  template: ` <p>event-form works!</p> `,
-  styles: [],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class EventFormComponent {}
--- src/app/features/event/event.module.ts
+++ src/app/features/event/event.module.ts
@@ -1,55 +1,26 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
-import { PowerIconComponent } from '@shared/ui';
-import { RouterModule } from '@angular/router';
-import { EventFormComponent } from './event-form/event-form.component';
-import { EventsComponent } from './events.component';
-import { HashtagsComponent } from './event-form/hashtags/hashtags.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { CardComponent } from '.';
-import { EventFormThemeComponent } from './event-form/event-form-theme.component';
-import { HighlightPipe } from '../../shared/pipes/hihglightPipe';
+import {
+  CardComponent,
+  EventApiService,
+  EventListComponent,
+  PowerIconComponent,
+} from '.';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    EventFormComponent,
-    EventsComponent,
-    HashtagsComponent,
-    CardComponent,
-    PowerIconComponent,
-    EventFormThemeComponent,
-  ],
+  declarations: [CardComponent, PowerIconComponent, EventListComponent],
   imports: [
-    MatInputModule,
-    MatButtonModule,
     MatIconModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatChipsModule,
-    MatAutocompleteModule,
-    ReactiveFormsModule,
-    CommonModule,
-    HighlightPipe,
     RouterModule.forChild([
       {
         path: '',
-        component: EventsComponent,
-      },
-      {
-        path: 'new-event',
-        component: EventFormComponent,
-      },
-      {
-        path: 'new-event-theme',
-        component: EventFormThemeComponent,
+        component: EventListComponent,
       },
     ]),
+    CommonModule,
   ],
   exports: [CardComponent, PowerIconComponent],
   providers: [EventApiService],
