689048a5615afadc792fc439f1db4b7cd801c5ea
Dominika Papierska <domi.papierska@gmail.com>
2023-02-14 09:33:13 +0100

    fix: duplicated imports, service providers
    removed dupliated imports in event module, changed providers for event api service

--- src/app/features/event/event.module.ts
+++ src/app/features/event/event.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 
-import { CommonModule, DatePipe, NgClass, NgIf, UpperCasePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { CardComponent, EventListComponent } from '.';
-import { PowerIconComponent } from './power-icon/power-icon.component';
+import {
+  CardComponent,
+  EventApiService,
+  EventListComponent,
+  PowerIconComponent,
+} from '.';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CardComponent, PowerIconComponent, EventListComponent],
   imports: [
-    NgIf,
-    NgClass,
-    DatePipe,
-    UpperCasePipe,
     MatIconModule,
     RouterModule.forChild([
       {
@@ -23,5 +23,6 @@ import { RouterModule } from '@angular/router';
     CommonModule,
   ],
   exports: [CardComponent, PowerIconComponent],
+  providers: [EventApiService],
 })
 export default class EventModule {}
