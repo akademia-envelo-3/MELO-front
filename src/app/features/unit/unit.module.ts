87ddf81b951c7ec21624ac1fd0d4ed9a83603475
Dominika Papierska <domi.papierska@gmail.com>
2023-03-07 13:54:31 +0100

    refactor: unused import
    removed mat snack bar import from unit module

--- src/app/features/unit/unit.module.ts
+++ src/app/features/unit/unit.module.ts
@@ -9,7 +9,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { UnitPowerIconComponent, UnitCardComponent, UnitFormComponent } from '.';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { SpinnerDotsComponent } from '@shared/ui';
 
 @NgModule({
