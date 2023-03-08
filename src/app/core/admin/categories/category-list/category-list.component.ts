7caba31537d9a76b6a08b76ebcc0219583d2754e
Dominika Papierska <domi.papierska@gmail.com>
2023-03-08 11:08:48 +0100

    feat: add category button
    added button with text to add new vategory, created new layout class of inner-container

--- src/app/core/admin/categories/category-list/category-list.component.ts
+++ src/app/core/admin/categories/category-list/category-list.component.ts
@@ -5,11 +5,29 @@ import { CategoriesApiService } from '../categories-api.service';
   selector: 'app-category-list',
   template: `
     <h1 class="text-h1 header-with-underline">Lista kategorii</h1>
+    <div class="add-category-container inner-container">
+      <p class="add-category-container__text text-body-big font-semibold">
+        Dodaj kategorię <app-circular-button size="sm" icon="add"></app-circular-button>
+      </p>
+    </div>
     <ng-container *ngIf="categories$ | async as categories">
       <app-table-items [itemsData]="categories"></app-table-items>
     </ng-container>
   `,
-  styles: [''],
+  styles: [
+    `
+      .add-category-container {
+        display: flex;
+        justify-content: flex-end;
+      }
+      .add-category-container__text {
+        display: inline-flex;
+        gap: 1rem;
+        align-items: center;
+        margin-top: 1rem;
+      }
+    `,
+  ],
   providers: [CategoriesApiService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
