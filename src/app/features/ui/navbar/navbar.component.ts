93c67047f6d59ace92de93cb6d576634c4e8dfe8
abdevworks <czasacz@gmail.com>
2023-03-09 11:12:38 +0100

    style: navbar-searchbar-filters
    Fixing styling

--- src/app/features/ui/navbar/navbar.component.ts
+++ src/app/features/ui/navbar/navbar.component.ts
@@ -4,10 +4,19 @@ import { Router } from '@angular/router';
 import { NgIf, NgClass, AsyncPipe } from '@angular/common';
 import { NotificationsQuantityComponent } from './notifications-quantity.component';
 import { SearchbarComponent } from '../searchbar-ui/searchbar/searchbar.component';
+import { FiltrSortButtonOutput } from '../filter-search-button';
+import { FilterSearchButtonComponent } from '../filter-search-button';
 
 @Component({
   standalone: true,
-  imports: [NotificationsQuantityComponent, NgIf, NgClass, SearchbarComponent, AsyncPipe],
+  imports: [
+    NotificationsQuantityComponent,
+    NgIf,
+    NgClass,
+    SearchbarComponent,
+    AsyncPipe,
+    FilterSearchButtonComponent,
+  ],
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.scss'],
@@ -24,5 +33,10 @@ export class NavbarComponent {
 
   navToHome() {
     this.router.navigate(['']);
+  }
+
+  eventSortFiltr(arg: FiltrSortButtonOutput<'events'>) {
+    arg.nameSort;
+    arg.dateSort;
   }
 }
